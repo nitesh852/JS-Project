fc3ab771d9ae33e5986210ef8c27fdf7d6b84ac4
nitesh852 <niteshbhatti852@gmail.com>
2022-09-22 23:22:20 +0530

    quizApp done

--- quizApp/index.js
+++ quizApp/index.js
@@ -33,36 +33,68 @@ const quizData =[
     },
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitBtn  = document.getElementById("submit");
-let currentQuiz =0;
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
 loadQuiz();
 
-function loadQuiz(){
-const curentQuizData = quizData[currentQuiz];
-questionEl.innerHTML = curentQuizData.question;
-a_text.innerText = curentQuizData.a;
-b_text.innerText = curentQuizData.b;
-c_text.innerText = curentQuizData.c;
-d_text.innerText = curentQuizData.d;
+function loadQuiz() {
+    deselectAnswers();
 
-currentQuiz++;
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
 
-submitBtn.addEventListener("click",() => {
-    currentQuiz++;
+function getSelected() {
+    let answer = undefined;
 
-    if(currentQuiz < quizData.length){
-        loadQuiz();
-    }
-    else
-    {
-        alert("You finished")
-    }
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
 
-    loadQuiz();
-})
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
